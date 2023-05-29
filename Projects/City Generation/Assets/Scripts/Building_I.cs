c76c3a783f38c720896a1ef94ac5ccb20520d63b
Yichen Mo <amomorning@gmail.com>
2023-05-29 17:24:46 +0800

    Add images

--- Projects/City Generation/Assets/Scripts/Building_I.cs
+++ Projects/City Generation/Assets/Scripts/Building_I.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mola;
 
+public enum BuildingType
+{
+    None,
+    Office,
+    House,
+    Museum,
+    Park
+}
+
 public class Building_I : MolaMonoBehaviour
 {
-    [Range(3, 200)]
-    public float dX = 10;
-    [Range(3, 200)]
-    public float dY = 15;
+    public BuildingType buildingType = new();
+
+    [Range(3, 20)]
+    public float dX = 16;
+    [Range(3, 20)]
+    public float dY = 16;
     [Range(3, 500)]
     public float dZ = 16;
     public MolaMesh startMesh;
@@ -16,55 +27,144 @@ public class Building_I : MolaMonoBehaviour
     [Range(0, 3)]
     public float extrudeHeight = 2;
 
+    public MolaMesh scaledMesh(MolaMesh mesh, float dx, float dy, float dz)
+    {
+        MolaMesh ret = mesh.Copy();
+        Vec3 center = new Vec3();
+        for (int i = 0; i < mesh.VertexCount(); ++i)
+        {
+            center += mesh.Vertices[i];
+        }
+        center /= mesh.VertexCount();
+
+        ret.Translate(-center.x, -center.y, -center.z);
+        ret.Scale(dx, dy, dz);
+        ret.Translate(center.x, center.y, center.z);
+
+        return ret;
+    }
+
     public override void UpdateGeometry()
     {
         // 2 methods: get input startmesh or create a quad with dX, dY
+        MolaMesh volume = new MolaMesh();
         if (startMesh == null)
         {
             startMesh = MeshFactory.CreateQuad(dX, dY);
+            volume = startMesh.Copy();
+
         }
-
-        // create 1st level of LOD mesh and assign material
-        MolaMesh volume = startMesh;
-        volume = MeshSubdivision.SubdivideMeshExtrude(volume, dZ);
-        AddLODMesh(volume, "Blue"); // you could add a single mesh and one material name of the material in Resources folder
-
-        // create 2nd level of LOD mesh and assign material
-        MolaMesh roof = volume.CopySubMesh(volume.FacesCount() - 1); // copy last face
-        MolaMesh wall = volume.CopySubMesh(volume.FacesCount() - 1, true); // copy faces of the rest
-
-        wall = MeshSubdivision.SubdivideMeshGrid(wall, 5, 5);
-        List<int> indexList = new List<int>();
-        Random.InitState(0);
-        for (int i = 0; i < wall.Faces.Count; i++)
+        else
         {
-            if (Random.value > 0.5f)
-            {
-                indexList.Add(i);
-            }
+            volume = scaledMesh(startMesh, dX / 20, dY / 20, 1f);
         }
 
-        MolaMesh newWall = wall.CopySubMesh(indexList); // copy faces by index list
-        wall = wall.CopySubMesh(indexList, true); // copy the inverted index list
+        //if (buildingType == BuildingType.None) { buildingType = BuildingType.Office; }
+        if (buildingType != BuildingType.None || buildingType != BuildingType.Park)
+        {
+            volume = MeshSubdivision.SubdivideMeshExtrude(volume, dZ);
+            AddLODMesh(volume, "Blue"); // you could add a single mesh and one material name of the material in Resources folder
 
-        newWall = MeshSubdivision.SubdivideMeshExtrude(newWall, extrudeHeight);
+        }
 
-        MolaMesh newRoof = newWall.CopySubMeshByNormalZ(0.1f, 1.1f); // face facing up with normalz 1, facing down with normalz -1, vertial 0 
-        newWall = newWall.CopySubMeshByNormalZ(0.1f, 1.1f, false, true); // copy the rest
+        MolaMesh wall = new MolaMesh();
+        MolaMesh roof = new MolaMesh();
+        MolaMesh window = new MolaMesh();
+        switch (buildingType)
+        {
+            case BuildingType.Office:
 
-        roof.AddMesh(newRoof);
-        wall.AddMesh(newWall);
+                // create 2nd level of LOD mesh and assign material
+                roof = volume.CopySubMesh(volume.FacesCount() - 1); // copy last face
+                wall = volume.CopySubMesh(volume.FacesCount() - 1, true); // copy faces of the rest
 
-        wall = MeshSubdivision.SubdivideMeshLinearSplitQuad(wall, 0.5f, 2f);
-        wall = MeshSubdivision.SubdivideMeshExtrudeTapered(wall, 0, 0.2f);
+                wall = MeshSubdivision.SubdivideMeshSplitGridAbs(wall, 5f, 5f);
+                List<int> indexList = new List<int>();
+                Random.InitState(0);
+                for (int i = 0; i < wall.Faces.Count; i++)
+                {
+                    if (Random.value > 0.5f)
+                    {
+                        indexList.Add(i);
+                    }
+                }
 
-        MolaMesh window = wall.CopySubMeshByModulo(4, 5); // get every 5th face
-        wall = wall.CopySubMeshByModulo(4, 5, true); // get the rest
+                MolaMesh newWall = wall.CopySubMesh(indexList); // copy faces by index list
+                wall = wall.CopySubMesh(indexList, true); // copy the inverted index list
 
-        // you could add a mesh list and a material name list of the material in Resources folder
-        AddLODMeshes(new List<MolaMesh> { wall, roof, window }, new List<string> { "Blue", "Green", "Glass" });
+                newWall = MeshSubdivision.SubdivideMeshExtrude(newWall, extrudeHeight);
 
-        // this step to add all lod mola meshes to unity object
+                MolaMesh newRoof = newWall.CopySubMeshByNormalZ(0.1f, 1.1f); // face facing up with normalz 1, facing down with normalz -1, vertial 0 
+                newWall = newWall.CopySubMeshByNormalZ(0.1f, 1.1f, false, true); // copy the rest
+
+                roof.AddMesh(newRoof);
+                wall.AddMesh(newWall);
+
+                wall = MeshSubdivision.SubdivideMeshLinearSplitQuad(wall, 0.5f, 2f);
+                wall = MeshSubdivision.SubdivideMeshExtrudeTapered(wall, 0, 0.2f);
+
+                window = wall.CopySubMeshByModulo(4, 5); // get every 5th face
+                wall = wall.CopySubMeshByModulo(4, 5, true); // get the rest
+                break;
+            case BuildingType.Museum:
+                volume = volume.CopySubMesh(volume.FacesCount() - 1);
+                volume.Translate(0, 0, -dZ);
+
+
+                float cur = 0.0f;
+                while (cur < dZ)
+                {
+                    float scale = Random.Range(0.2f, 1.1f);
+                    float height = 10f * scale;
+
+                    MolaMesh layer = scaledMesh(volume, scale, scale, 1f);
+                    MolaMesh floor = layer.Copy();
+                    floor.FlipFaces();
+                    floor.Translate(0, 0, cur);
+                   
+                    
+                    roof.AddMesh(floor);
+
+                    layer = MeshSubdivision.SubdivideMeshExtrude(layer, height);
+                    layer.Translate(0, 0, cur);
+                    MolaMesh roofT = layer.CopySubMesh(layer.FacesCount() - 1); // copy last face
+                    MolaMesh wallT = layer.CopySubMesh(layer.FacesCount() - 1, true); // copy faces of the rest
+
+                    wallT = MeshSubdivision.SubdivideMeshSplitGridAbs(wallT,6f, 10f);
+
+                    wallT = MeshSubdivision.SubdivideMeshOffsetPerEdge(wallT, new float[] { -0.2f, -2f, -height*0.9f, -2f });
+
+                    MolaMesh windowT = wallT.CopySubMeshByModulo(2, 5, true);
+                    wallT = wallT.CopySubMeshByModulo(2, 5);
+
+                    roofT = MeshSubdivision.SubdivideMeshExtrude(roofT, 0.5f, true);
+                    roof.AddMesh(roofT);
+                    wall.AddMesh(wallT);
+                    window.AddMesh(windowT);
+                    cur += height;
+                }
+
+                break;
+            case BuildingType.House:
+
+                roof = volume.CopySubMesh(volume.FacesCount() - 1); // copy last face
+                wall = volume.CopySubMesh(volume.FacesCount() - 1, true); // copy faces of the rest
+                roof = MeshSubdivision.SubdivideMeshSplitRoof(roof, 2f);
+                wall = MeshSubdivision.SubdivideMeshSplitGridAbs(wall, 7.2f, 3.6f);
+
+                wall = MeshSubdivision.SubdivideMeshOffsetPerEdge(wall, new float[] { -0.2f, -2f, -2f, -1f });
+                window = wall.CopySubMeshByModulo(4, 5); // get every 5th face
+                wall = wall.CopySubMeshByModulo(4, 5, true); // get the rest
+                break;
+
+            case BuildingType.Park:
+                break;
+            default:
+                break;
+        }
+
+        AddLODMeshes(new List<MolaMesh> { wall, roof, window }, new List<string> { "White", "Concrete", "Glass" });
         AddLODsToObject();
+
     }
 }
--- Projects/City Generation/Assets/Scripts/PlotSubdivide_I.cs
+++ Projects/City Generation/Assets/Scripts/PlotSubdivide_I.cs
@@ -9,8 +9,13 @@ public class PlotSubdivide_I : MolaMonoBehaviour
     public float dX = 206;
     [Range(10, 300)]
     public float dY = 120;
-    [Range(6, 50)]
+    [Range(4, 18)]
     public float streetwidth = 7;
+
+    [Range(0, 5)]
+    public int numMuseum = 1;
+    [Range(0.1f, 0.8f)]
+    public float ratioHouse = 0.4f;
 
 
     override public void UpdateGeometry()
@@ -20,16 +25,27 @@ public class PlotSubdivide_I : MolaMonoBehaviour
 
         // subdivide plot into smaller plots and street
         MolaMesh plot = MeshFactory.CreateQuad(dX, dY);
-        plot = MeshSubdivision.SubdivideMeshOffsetPerEdge(plot, new float[] { -2f, -1f, -4f, -6f });
-        plot = MeshSubdivision.SubdivideMeshGrid(plot, 1, 3);
+        plot = MeshSubdivision.SubdivideMeshOffsetPerEdge(plot, new float[] { -50f, -40f, -40f, -60f });
+        plot = MeshSubdivision.SubdivideMeshSplitRelative(plot, 0, 0.6f, 0.9f, 0.3f, 0.5f);
 
-        plot = MeshSubdivision.SubdivideMeshOffset(plot, streetwidth * -0.5f);
+        MolaMesh newplot = new MolaMesh();
+        for(int i = 0; i < plot.FacesCount(); ++ i) {
+            if (plot.FaceArea(i) > 6000) {
+                List<Vec3[]> new_faces_vertices = FaceSubdivision.Grid(plot.FaceVertices(i), 2, 2);
+                newplot.AddFaces(new_faces_vertices);
+            } else { 
+                newplot.AddFace(plot.FaceVertices(i));
+            }
+        }
 
-        MolaMesh plots = plot.CopySubMeshByModulo(3, 4); // get every 4th face
-        MolaMesh street = plot.CopySubMeshByModulo(3, 4, true); // get the rest faces
+        plot = MeshSubdivision.SubdivideMeshOffset(newplot, streetwidth * -0.5f);
+
+        MolaMesh plots = plot.CopySubMeshByModulo(4, 5); // get every 4th face
+        MolaMesh street = plot.CopySubMeshByModulo(4, 5, true); // get the rest faces
+
 
         List<MolaMesh> meshes = new List<MolaMesh> { plots, street };
-        List<string> materials = new List<string> { "Green", "Grey" };
+        List<string> materials = new List<string> { "Green", "Grey"};
 
         // new method of add mola mesh to unity object with LOD embedded
         AddLODMeshes(meshes, materials);
@@ -37,15 +53,61 @@ public class PlotSubdivide_I : MolaMonoBehaviour
 
         // for each face of plots, create a new game object, attach your building script
         //Debug.Log(transform.position);
+
+        List<System.Tuple<int, float>> ls = new List<System.Tuple<int, float>>();
+        for (int i = 0; i < plots.FacesCount(); ++ i) {
+            ls.Add(new System.Tuple<int, float>(i, -faceArea(plots.FaceVertices(i)) / plots.FaceBoundingBox(i).GetAreaXY()));
+        }
+        ls.Sort((x, y) => x.Item2.CompareTo(y.Item2));
+
+        int[] st = new int[numMuseum];
+        for (int i = 0; i < numMuseum; ++ i) {
+            st[i] = ls[i].Item1;
+        }
+
+
         for (int i = 0; i < plots.FacesCount(); i++)
         {
             GameObject buildingObject = new GameObject();
             buildingObject.transform.parent = this.transform;
             buildingObject.transform.localPosition = new Vector3(0, 0, 0); // if the plot is not at origin, need to reset child local position.
 
+
+
             var buildingscript = buildingObject.AddComponent<Building_I>();
             buildingscript.startMesh = plots.CopySubMesh(i);
             buildingscript.dZ = Random.Range(5, 100);
+
+            float rng = Random.Range(0f, 1f);
+            if (rng < ratioHouse) {
+                buildingscript.buildingType = BuildingType.House;
+            } else { 
+                buildingscript.buildingType = BuildingType.Office;
+            }
+
+
+            bool flag = false;
+            foreach (var x in st) {
+                if (i == x) flag = true;
+            }
+            if (flag) { 
+                buildingscript.buildingType = BuildingType.Museum;
+            }
         }
+    }
+
+    public float faceArea(Vec3[] vertices) {
+        float ret = 0;
+        for(int i = 1; i < vertices.Length-1; ++ i) { 
+          Vec3 a = vertices[i] - vertices[0];
+          Vec3 b = vertices[i+1] - vertices[0];
+            ret += Mola.Mathf.Abs(crossXY(a, b) / 2);
+        }
+        return ret;
+    }
+
+    public float crossXY(Vec3 a, Vec3 b)
+    {
+        return a.x * b.y - a.y * b.x;
     }
 }
